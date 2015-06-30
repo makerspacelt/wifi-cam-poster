ca1a1c9dff65dd536ab3651df4304a476d149fbc
darius <darius@vozbutas.lt>
2015-06-30 22:16:22 +0300

    refactoring downloader and implementing poster to tumblr
    * making class out of get_photos script (needs more refactoring)
    * implementing FilenameManager to make best effor extracting date of photo
      and naming file accordingly
    * implementing PosterTumblr for easy posting photos to tumblr
    * adding Config.dist.php to configure all aspects of the script
    * adding run.php to construct necessary objects and make it all work
    * adding build.php to make a one step build of a self contained phar archive

---
+++ Config.dist.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class Config {
+
+	public static $photoPath      = REAL_DIR;
+	public static $ezHost         = '192.168.4.1:80';
+	public static $ezDir          = '100CANON';
+
+	public static $blogName       = 'tumblr_blog_name';
+	public static $consumerKey    = '';
+	public static $consumerSecret = '';
+	public static $token          = '';
+	public static $tokenSecret    = '';
+
+}
+
